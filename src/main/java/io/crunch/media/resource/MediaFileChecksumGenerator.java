0cc01d135a3c53bbcd090b2d2c3cffa966ec0015
Andras Fejes <afejes@liveperson.com>
2025-02-07 13:07:52 +0100

    ✨Added checksum generator

---
+++ src/main/java/io/crunch/media/resource/MediaFileChecksumGenerator.java
@@ -0,0 +1,25 @@
+package io.crunch.media.resource;
+
+import io.crunch.media.shared.MediaFileServerException;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+
+/**
+ * Generates a checksum value of the provided file by using SHA256 hash algorithm.
+ */
+@ApplicationScoped
+public class MediaFileChecksumGenerator {
+
+    public String checksum(Path path) {
+        try (var fis = new BufferedInputStream(new FileInputStream(path.toFile()))) {
+            return DigestUtils.sha256Hex(fis);
+        } catch (IOException e) {
+            throw new MediaFileServerException(e);
+        }
+    }
+}
---
+++ src/test/java/io/crunch/media/resource/MediaFileChecksumGeneratorTest.java
@@ -0,0 +1,29 @@
+package io.crunch.media.resource;
+
+import io.crunch.media.shared.MediaFileServerException;
+import org.junit.jupiter.api.Test;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class MediaFileChecksumGeneratorTest {
+
+    @Test
+    void generateChecksumWhenFileExists() throws URISyntaxException {
+        var url = MediaFileChecksumGeneratorTest.class.getResource("/sample-pdf.pdf");
+        var path = Path.of(Objects.requireNonNull(url).toURI());
+        var generator = new MediaFileChecksumGenerator();
+        assertThat(generator.checksum(path)).isEqualTo("38c9792d725c45dd431699e6a3b0f0f8e17c63c9ac7331387ee30dcc6e42a511");
+    }
+
+    @Test
+    void throwsErrorIfFileDoesNotExists() {
+        var generator = new MediaFileChecksumGenerator();
+        var path = Path.of("does-not-exist");
+        assertThatThrownBy(() -> generator.checksum(path)).isInstanceOf(MediaFileServerException.class);
+    }
+}
